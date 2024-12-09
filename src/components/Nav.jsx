45b7fc559a23cd7c2119167ee1757409f8fb1fe4
Ali Hassan <i221545@nu.edu.pk>
2024-12-09 19:39:37 +0500

    dddd

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,34 +8,42 @@ import { BsFillClipboard2CheckFill } from "react-icons/bs";
 const Nav = () => {
   return (
     <nav
-      className="nav flex w-2/3 gap-2 border-2 border-black justify-between px-6 py-2 sm:w-1/2 md:w-2/5 lg:w-1/4 xl:w-1/5 bg-white shadow-lg rounded-full fixed bottom-3 left-1/2 transform -translate-x-1/2 text-black"
+      className="nav flex w-2/3 gap-2 border-2 border-black justify-between px-6 py-2 sm:w-1/2 md:w-2/5 lg:w-1/4 xl:w-1/5 bg-white shadow-lg rounded-full fixed bottom-3 left-1/2 transform -translate-x-1/2"
     >
-      <Link to="/" aria-label="Home" className="flex flex-col items-center">
-        <button className="flex flex-col gap-1 items-center bg-transparent border-none text-black hover:underline">
-          <MdDashboard className="text-black text-base sm:text-lg md:text-xl lg:text-2xl" />
-          <span className="text-sm sm:text-base font-semibold">Home</span>
-        </button>
+      <Link
+        to="/"
+        aria-label="Home"
+        className="flex flex-col gap-1 items-center cursor-pointer hover:underline"
+      >
+        <MdDashboard className="text-lg" />
+        <span className="under text-sm font-semibold">Home</span>
       </Link>
 
-      <Link to="/groups" aria-label="Groups" className="flex flex-col items-center">
-        <button className="flex flex-col gap-1 items-center bg-transparent border-none text-black hover:underline">
-          <HiUserGroup className="text-black text-base sm:text-lg md:text-xl lg:text-2xl" />
-          <span className="text-sm sm:text-base font-semibold">Groups</span>
-        </button>
+      <Link
+        to="/groups"
+        aria-label="Groups"
+        className="flex flex-col gap-1 items-center cursor-pointer hover:underline"
+      >
+        <HiUserGroup className="text-lg" />
+        <span className="under text-sm font-semibold">Groups</span>
       </Link>
 
-      <Link to="/approvals" aria-label="Approvals" className="flex flex-col items-center">
-        <button className="flex flex-col gap-1 items-center bg-transparent border-none text-black hover:underline">
-          <BsFillClipboard2CheckFill className="text-black text-base sm:text-lg md:text-xl lg:text-2xl" />
-          <span className="text-sm sm:text-base font-semibold">Approvals</span>
-        </button>
+      <Link
+        to="/approvals"
+        aria-label="Approvals"
+        className="flex flex-col gap-1 items-center cursor-pointer hover:underline"
+      >
+        <BsFillClipboard2CheckFill className="text-lg" />
+        <span className="under text-sm font-semibold">Approvals</span>
       </Link>
 
-      <Link to="/profile" aria-label="Profile" className="flex flex-col items-center">
-        <button className="flex flex-col gap-1 items-center bg-transparent border-none text-black hover:underline">
-          <FaUserCog className="text-black text-base sm:text-lg md:text-xl lg:text-2xl" />
-          <span className="text-sm sm:text-base font-semibold">Profile</span>
-        </button>
+      <Link
+        to="/profile"
+        aria-label="Profile"
+        className="flex flex-col gap-1 items-center cursor-pointer hover:underline"
+      >
+        <FaUserCog className="text-lg" />
+        <span className="under text-sm font-semibold">Profile</span>
       </Link>
     </nav>
   );
